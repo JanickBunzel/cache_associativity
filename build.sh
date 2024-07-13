bfcb3c4692b8d863273433351e7927ab5ebc537f
Janick <43705189+JanickBunzel@users.noreply.github.com>
2024-07-13 16:14:20 +0200

    Parallel compilation to prevent Artemis timeout (#10)
    * Makefile optimize: Parallel compilation, object files

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 echo "Running build.sh from gra24cdaproject-g157:"
 
 echo "(1) Compiling..."
-make all
-echo "(1) Compilation done into ./cache_simulation"
 
+# Running make with 4 threads
+make -j4
+
+echo "(1) Compilation done into ./cache_simulation"
 echo "(2) Executing..."
+
 ./cache_simulation examples/test.csv
+
 echo "(2) Execution done"
