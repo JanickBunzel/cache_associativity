7c0f66732ca8e25811c79b2c791658f9ccfe6c40
Janick Bunzel <janick@bunzel-online.de>
2024-07-16 23:21:31 +0200

    cycles handling stop sim

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 Cpu::Cpu(sc_module_name name, Request *requests, const int requestLength)
-    : sc_module(name), cpuStatistics({0,0}), requests(requests), requestLength(requestLength)
+    : sc_module(name), cpuStatistics({0, 0}), requests(requests), requestLength(requestLength)
 {
     SC_THREAD(handleRequests);
     dont_initialize();
@@ -11,8 +11,8 @@ Cpu::Cpu(sc_module_name name, Request *requests, const int requestLength)
 
 void Cpu::handleRequests()
 {
-    bool cacheDone = false;
-  
+    unsigned cycles = 1;
+
     for (int i = 0; i < requestLength; i++)
     {
         cpuStatistics.requests++;
@@ -20,7 +20,6 @@ void Cpu::handleRequests()
         std::cout << "###########################################################################################################################################################################" << std::endl;
         std::cout << "[Cpu]: Request[" << i << "]: " << (requests[i].we ? "W," : "R,") << requests[i].addr << "," << (requests[i].we ? std::to_string(requests[i].data) : "") << std::endl;
         std::cout << "###########################################################################################################################################################################" << std::endl;
-
 
         // Send the request to the cache
         addressCPUOut.write(requests[i].addr);
@@ -31,7 +30,22 @@ void Cpu::handleRequests()
         while (cacheDone == false)
         {
             cpuStatistics.cycles++;
-            //std::cout << "[Cpu]: Zyklus: " << cpuStatistics.cycles << std::endl;
+            std::cout << "Cycles[" << cycles++ << "]" << std::endl;
+
+            // Wait for the Cpu-Cache signals
+            wait(SC_ZERO_TIME);
+            // Wait for the Cache-Memory signals
+            wait(SC_ZERO_TIME);
+            // Wait for the Memory-Cache signals
+            wait(SC_ZERO_TIME);
+            // Wait for the Cache-Cpu signals
+            wait(SC_ZERO_TIME);
+
+            if (cacheDoneCPUIn.read() == true && i == requestLength - 1)
+            {
+                sc_stop();
+            }
+
             wait();
             cacheDone = cacheDoneCPUIn.read();
         }
@@ -40,7 +54,6 @@ void Cpu::handleRequests()
         std::cout << "[Cpu]: Cache done processing request[" << i << "]" << std::endl;
         std::cout << std::hex << "[Cpu]: Read Data: " << cacheReadDataCPUIn.read() << std::dec << std::endl;
         std::cout << "###########################################################################################################################################################################" << std::endl;
-
 
         cacheDone = false;
     }
